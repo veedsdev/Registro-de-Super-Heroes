a26cfb4c5d94af9ccef9dbcfaee8b9adeb61e8d3
EDGAR VEGA ARAGON <ingvegaedgar@gmail.com>
2023-04-29 17:41:33 -0600

    sending data between main and detail activity

--- RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/DetailActivity.kt
+++ RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/DetailActivity.kt
@@ -2,10 +2,32 @@ package mx.veedsdev.com.registrodesuperheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.veedsdev.com.registrodesuperheroes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object{
+        const val SUPER_HERO_NAME_KEY="superHeroName"
+        const val ALTER_EGO_KEY="alterEgo"
+        const val BIOGRAPHY_KEY="biography"
+        const val POWER_KEY="power"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras!!
+        val superHeroName = bundle.getString(SUPER_HERO_NAME_KEY) ?: "No se encontró el nombre"
+        val alterEgo = bundle.getString(ALTER_EGO_KEY) ?: "No se encontró el Alter Ego"
+        val bio = bundle.getString(BIOGRAPHY_KEY) ?: "No se encontró el Alter Ego"
+        val power = bundle.getFloat(POWER_KEY)
+
+        binding.tvDetailHeroName.text = superHeroName
+        binding.tvDetailAlterEgoDescription.text = alterEgo
+        binding.tvDetailBiographyDescription.text = bio
+        binding.rbDetailPower.rating = power
+
     }
 }
--- RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/MainActivity.kt
+++ RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/MainActivity.kt
@@ -12,12 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnMainSave.setOnClickListener{
-            openDetailActivity()
+            val superHeroName = binding.etMainHeroName.text.toString()
+            val alterEgo = binding.etMainAlterEgo.text.toString()
+            val biography = binding.etMainBiography.text.toString()
+            val power = binding.rbMainPower.rating
+            openDetailActivity(superHeroName = superHeroName, alterEgo = alterEgo,biography=biography, power = power)
         }
     }
 
-    private fun openDetailActivity(){
+    private fun openDetailActivity(superHeroName : String, alterEgo : String, biography : String, power : Float){
         val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(DetailActivity.SUPER_HERO_NAME_KEY,superHeroName)
+        intent.putExtra(DetailActivity.ALTER_EGO_KEY,alterEgo)
+        intent.putExtra(DetailActivity.BIOGRAPHY_KEY,biography)
+        intent.putExtra(DetailActivity.POWER_KEY,power)
         startActivity(intent)
     }
 }
