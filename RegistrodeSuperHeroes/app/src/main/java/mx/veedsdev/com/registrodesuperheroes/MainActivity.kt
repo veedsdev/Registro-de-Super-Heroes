20b82d0ba057645898a59f8b1fd3672335598a54
EDGAR VEGA ARAGON <ingvegaedgar@gmail.com>
2023-04-29 17:08:19 -0600

    Explicit intent MainActivity to DetailActivity

--- RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/MainActivity.kt
+++ RegistrodeSuperHeroes/app/src/main/java/mx/veedsdev/com/registrodesuperheroes/MainActivity.kt
@@ -1,11 +1,23 @@
 package mx.veedsdev.com.registrodesuperheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.veedsdev.com.registrodesuperheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMainSave.setOnClickListener{
+            openDetailActivity()
+        }
+    }
+
+    private fun openDetailActivity(){
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 }
